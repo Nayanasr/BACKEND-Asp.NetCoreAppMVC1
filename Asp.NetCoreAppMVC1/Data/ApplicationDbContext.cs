c27e846f4fb313648da7c9adcc77c58b6a8efc15
Nayana Ramesh <107012@tfcorp.com>
2022-11-30 19:54:55 +0530

    added

---
+++ Asp.NetCoreAppMVC1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Asp.NetCoreAppMVC1.Models;
+
+namespace Asp.NetCoreAppMVC1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+        }
+        public DbSet<Asp.NetCoreAppMVC1.Models.Student> Student { get; set; }
+    }
+}
